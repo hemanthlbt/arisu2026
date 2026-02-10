a916596b8e0043e37fe9d7fbcfefbf121d2e31bd
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-10 16:18:42 +0530

    another file

---
+++ 5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+
+
+{
+
+int a = 1000;
+int b = 200;
+int c = 300;
+int d = 400;
+
+
+
+if (a==100) {
+
+printf("This is a prime number!!!\n");
+
+}
+
+
+else {
+
+
+printf("This is not a prime number\n");
+
+}
+
+
+
+return 0;
+
+
+
+
+}
