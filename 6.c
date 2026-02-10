8efabf4380e25ab5380d19636b5f060cef0cef17
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-10 18:17:41 +0530

    for loop1

---
+++ 6.c
@@ -0,0 +1,27 @@
+// print numbers from 1 to 10 using a for loop..
+
+
+#include <stdio.h>
+
+int main() 
+
+
+
+{
+
+
+	int i;
+
+
+	for (i=0; i <=70; i++) 
+	{
+	
+			printf("%d\n",i);
+	}
+
+
+
+
+return 0;
+
+}
