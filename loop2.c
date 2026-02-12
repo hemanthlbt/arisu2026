a4e9a135a12f3a1c12a0bcda564634312379ebba
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-13 00:14:08 +0530

    loops

---
+++ loop2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+
+
+{
+
+	int i,j,k;
+	float m,n,b;
+	
+
+
+
+	printf("Enter the first number\n");
+	scanf("%d",&i);
+
+	printf("Enter one float number\n");
+	scanf("%f",&m);
+
+
+	int sum  = i + m;
+	printf("The sum of the two numbers i and m is\n");
+	printf("Answer is %d\n", sum);
+
+
+
+
+return 0;
+
+}
