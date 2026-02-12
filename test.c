17e594ffd81c3060cbb301acaf7f900147a2c1e6
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-12 23:42:00 +0530

    test

---
+++ test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+
+
+int n;
+
+	printf("Enter the number\n");
+	scanf("%d",&n);
+if (n==100) {
+
+	printf("This is a great number");
+
+
+}
+
+else {
+
+printf("This is not a great number");
+
+}
+
+
+
+
+
+return 0;
+}
+
