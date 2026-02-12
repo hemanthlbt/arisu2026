9fa17ae7c4473b21d705b2e474197095652f9cc0
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-12 23:49:24 +0530

    this is a basic sum c program...

---
+++ add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+
+	 {
+	 
+	 
+	 int a;
+	 int b;
+
+
+
+	 printf("Enter the first number\n");
+	 scanf("%d",&a);
+	 printf("Enter the second number\n");
+	 scanf("%d",&b);
+
+
+		int sum  =  a +b;
+	 printf("The sum of the two numbers a and b is...\n");
+	 printf("The answer is %d\n", sum);
+
+
+
+
+
+
+
+	 
+	 
+	 
+	 
+	 
+	 return 0;
+	 
+	 
+	 }
