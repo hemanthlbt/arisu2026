bbb1b99c5828f2318e74b9462e411c9ea487bea0
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-14 17:23:36 +0530

    lasttime

---
+++ types.c
@@ -0,0 +1,32 @@
+// all the c programming integer and data types!!!!
+
+
+
+
+#include <stdio.h>
+int main()
+
+
+{
+
+
+
+int number1 = 100;
+float number = 9.0;
+char name  = 'r';
+
+
+printf("%c\n",name);
+printf("%d\n",number1);
+
+
+
+
+
+return 0;
+
+
+
+
+
+}
