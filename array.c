c4526c0fa07af993dd6f20e4a1f83628ea8325c2
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-18 17:03:07 +0530

    arrays

---
+++ array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+
+
+{
+
+
+
+int nums[10] = {1,2,3,4,5,6,7,8,9};
+int i;
+for (i=0;i <nums[i]; i++) {
+printf("%d\n",nums[i]);
+
+
+}
+
+
+
+
+
+
+
+return 0;
+
+
+}
