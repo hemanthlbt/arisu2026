ab9e50b51b1220a54b9e0d7384db69096fe86fc6
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-22 17:47:27 +0530

    push this

--- delhi.c
+++ delhi.c
@@ -3,7 +3,7 @@ int main() {
 
 
 printf("Life is getting more serious now a days\n");
-
+printf("Today I have worked soo hard on flutter and I have so much time left for me to complete\n");
 
 return 0;
 }
