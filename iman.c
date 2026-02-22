2195480d08702c40b50a421a5d0a8a9a00d15f31
hemanthkumarlingabathina <lingabathina@icloud.com>
2026-02-23 01:18:46 +0530

    thisistheend

---
+++ iman.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+
+	printf("This should be the lasst thing I've ever had a c on!\n");
+	printf("My be I should leave the thins as I kept in my mind!!!\n");
+
+
+
+	int date = 100;
+	int date2 = 200;
+	int date3 = 300;
+	int date4 = 400;
+
+
+	printf("%d\n%d\n%d\n%d\n",date,date2,date3,date4);
+
+
+	int r_date = 27062024;
+	int e_date = 000;
+
+	printf("I really hope it ends as I've expected, I really don't nee all of this pain ever!!\n");
+	printf("I really should focus on the other things man, I am not really liking this life!!\n");
+
+
+return 0;
+
+}
