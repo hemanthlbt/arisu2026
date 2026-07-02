0f4803379855759724a9affccef1f6e7df0f95ab
root <root@DESKTOP-E66BBTB.localdomain>
2026-07-02 05:41:53 +0000

    new stages

---
+++ k.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+
+printf("These is somethig else");
+
+
+
+
+return 0;
+
+}
---
+++ news.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+
+
+printf("Enter a number");
+
+
+
+printf("Enter two numbers");
+
+
+
+
+
+return 0;
+
+
+
+
+}
